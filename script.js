d92c3dcd78d096f09ff8a494eee6038743287ffb
L-VS <167241825+L-VS@users.noreply.github.com>
2024-11-10 12:15:22 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-// Exemple d'animation du bouton
-const buttons = document.querySelectorAll('.btn-main');
-buttons.forEach(button => {
-    button.addEventListener('mouseenter', () => {
-        button.style.transform = 'scale(1.1)';
-    });
-    button.addEventListener('mouseleave', () => {
-        button.style.transform = 'scale(1)';
-    });
-});
+
